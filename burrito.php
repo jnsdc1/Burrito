d3d43b229fe7e545861b858f465cd271030c9e3b
jnsdc1 <jnsdc@iup.edu>
2026-04-29 16:18:34 -0400

    Starting Php and HTML

---
+++ burrito.php
@@ -0,0 +1,8 @@
+<html> 
+    <body> 
+        <p>
+            <?php
+            $mealType = $_POST["mealType"];
+            $beans = $_POST["Beans"];
+            $protein = $_POST["Protein"];
+            
