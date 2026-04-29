aaa91ff2df4994e9006a6105be92d17df6ccba4d
jnsdc1 <jnsdc@iup.edu>
2026-04-29 16:32:37 -0400

    All Im doing today
    Added HTML forms for Meal Type to the HTML file and assigned prices according to which option is selected on the PHP file.

--- burrito.php
+++ burrito.php
@@ -2,7 +2,19 @@
     <body> 
         <p>
             <?php
-            $mealType = $_POST["mealType"];
+            $mealType = $_POST["MealType"];
             $beans = $_POST["Beans"];
             $protein = $_POST["Protein"];
-            
+
+            if ($mealType == "Burrito"){
+                $price = 10.25;
+            }
+
+            elseif ($mealType == "Bowl"){
+                $price = 10.50;
+            }
+
+            elseif ($mealType == "Taco"){
+                $price = 8.75
+            }
+
