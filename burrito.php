872442ee492f641a21a438a28f2c0705eb3dbc93
jnsdc1 <jnsdc@iup.edu>
2026-04-30 13:45:47 -0400

    Added toppings and working on creating receipt

--- burrito.php
+++ burrito.php
@@ -6,6 +6,7 @@
             $beans = $_POST["Beans"];
             $rice = $_POST["Rice"];
             $protein = $_POST["Protein"];
+            $toppings =$_POST["Topping"];
 
             if ($mealType == "Burrito"){
                 $price = 10.25;
@@ -18,4 +19,24 @@
             elseif ($mealType == "Taco"){
                 $price = 8.75
             }
+
+            if (in_array('Queso', $toppings)){
+                $price += 2.00;
+            }
+
+            if (in_array('Guac',$toppings)){
+                $price +=2.00;
+            }
+
+            echo $price;
+            for  ($i =0; $i < count($toppings); $i++){
+                echo $toppings[$i] . "<br>";
+            }
+            ?>
+        </p>
+    </body>
+</html>
+
+
+
 
